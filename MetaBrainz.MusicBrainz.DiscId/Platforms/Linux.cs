017b879752a262b3def4f027dc1ca5ee33a0457f
Tim Van Holder <tim.vanholder@gmail.com>
2016-05-10 00:25:20 +0200

    Implemented Unix platform selection (using uname()). Implemented GetDeviceByIndex() for Linux.

--- MetaBrainz.MusicBrainz.DiscId/Platforms/Linux.cs
+++ MetaBrainz.MusicBrainz.DiscId/Platforms/Linux.cs
@@ -1,10 +1,33 @@
-﻿namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
+﻿using System;
+using System.IO;
+
+namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
 
   internal sealed class Linux : Unix {
 
+    public Linux() : base(CdDeviceFeature.ReadTableOfContents | CdDeviceFeature.ReadMediaCatalogNumber | CdDeviceFeature.ReadTrackIsrc) { }
+
     public override string DefaultDevice => "/dev/cdrom";
 
-    // TODO: Port disc_linux.c
+    public override string GetDeviceByIndex(int n) {
+      if (n < 0)
+        return null;
+      using (var info = System.IO.File.OpenText("/proc/sys/dev/cdrom/info")) {
+        string line;
+        while ((line = info.ReadLine()) != null) {
+          if (line.StartsWith("drive name:")) {
+            var devices = line.Substring(11).Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            if (n >= devices.Length)
+              return null;
+            Array.Reverse(devices);
+            return string.Concat("/dev/", devices[n]);
+          }
+        }
+      }
+      return null;
+    }
+
+    // TODO: Port the rest of disc_linux.c
 
   }
 
--- MetaBrainz.MusicBrainz.DiscId/Platforms/Unix.cs
+++ MetaBrainz.MusicBrainz.DiscId/Platforms/Unix.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Runtime.InteropServices;
 
 namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
 
@@ -6,9 +8,29 @@ namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
 
     protected Unix(CdDeviceFeature features = CdDeviceFeature.ReadTableOfContents) : base(features) { }
 
-    public new static Unix Create() {
-      // TODO: Detect FreeBSD/NetBSD/Solaris and return the appropriate subclass.
-      return new Linux();
+    [DllImport("libc")]
+    private static extern int uname(byte[] data);
+
+    public new static IPlatform Create() {
+      var os = string.Empty;
+      { // uname() technically fills a struct with multiple arrays of fixed-but-undefined size.
+        // However, the arrays are guaranteed to be NUL-terminated, and since there's only 6 of them, 8K should be plenty.
+      var buf = new byte[8 * 1024];
+        if (uname(buf) == 0) {
+        var endpos = Array.IndexOf<byte>(buf, 0);
+          if (endpos >= 0)
+            os = Encoding.ASCII.GetString(buf, 0, endpos); // FIXME: Or Encoding.Default?
+        }
+      }
+      switch (os) {
+        case "FreeBSD": return new FreeBsd    ();
+        case "NetBSD" : return new NetBsd     ();
+        case "OpenBSD": return new NetBsd     ();
+        case "Linux"  : return new Linux      ();
+        case "Darwin" : // OSX is not currently supported (no access to it)
+        case "SunOS"  : // Solaris is not currently supported (couldn't get Mono to work on it) 
+        default:        return new Unsupported();
+      }
     }
 
     public override string DefaultDevice => "/dev/cdrom";
