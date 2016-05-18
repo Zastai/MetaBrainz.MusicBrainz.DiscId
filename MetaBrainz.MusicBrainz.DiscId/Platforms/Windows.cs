80c135c58526e236215743a1e2ec3721c2698110
Tim Van Holder <tim.vanholder@gmail.com>
2016-05-18 20:00:31 +0200

    Restructured the Windows platform to match the Linux one.

--- MetaBrainz.MusicBrainz.DiscId/Platforms/Windows.cs
+++ MetaBrainz.MusicBrainz.DiscId/Platforms/Windows.cs
@@ -30,21 +30,13 @@ namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
     public override TableOfContents ReadTableOfContents(string device, CdDeviceFeature features) {
       if (device == null)
         device = this.GetDeviceByIndex(0) ?? this.DefaultDevice;
-      using (var hDevice = Windows.CreateDeviceHandle(device)) {
+      using (var hDevice = NativeApi.OpenDevice(device)) {
         byte first = 0;
         byte last  = 0;
         TableOfContents.RawTrack[] tracks = null;
         { // Read the TOC itself
-          var req = new TOCRequest(MMC3.TOCRequestFormat.TOC);
-          var rawtoc = new MMC3.TOCDescriptor();
-          var returned = 0;
-          // LIB-44: Apparently for some multi-session discs, the first TOC read can be wrong. So issue two reads.
-          var ok = Windows.DeviceIoControl(hDevice, Windows.IOCTL_CDROM_READ_TOC_EX, ref req, Marshal.SizeOf(req), out rawtoc, Marshal.SizeOf(rawtoc), out returned, IntPtr.Zero);
-          if (ok)
-            ok = Windows.DeviceIoControl(hDevice, Windows.IOCTL_CDROM_READ_TOC_EX, ref req, Marshal.SizeOf(req), out rawtoc, Marshal.SizeOf(rawtoc), out returned, IntPtr.Zero);
-          if (!ok)
-            throw new IOException("Failed to retrieve TOC.", new Win32Exception(Marshal.GetLastWin32Error()));
-          rawtoc.FixUp(req.AddressAsMSF);
+          MMC3.TOCDescriptor rawtoc;
+          NativeApi.GetTableOfContents(hDevice, out rawtoc);
           first = rawtoc.FirstTrack;
           last = rawtoc.LastTrack;
           tracks = new TableOfContents.RawTrack[last + 1];
@@ -52,7 +44,7 @@ namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
           for (var trackno = rawtoc.FirstTrack; trackno <= rawtoc.LastTrack; ++trackno, ++i) { // Add the regular tracks.
             if (rawtoc.Tracks[i].TrackNumber != trackno)
               throw new InvalidDataException($"Internal logic error; first track is {rawtoc.FirstTrack}, but entry at index {i} claims to be track {rawtoc.Tracks[i].TrackNumber} instead of {trackno}");
-            var isrc = ((features & CdDeviceFeature.ReadTrackIsrc) != 0) ? Windows.GetTrackIsrc(hDevice, trackno) : null;
+            var isrc = ((features & CdDeviceFeature.ReadTrackIsrc) != 0) ? NativeApi.GetTrackIsrc(hDevice, trackno) : null;
             tracks[trackno] = new TableOfContents.RawTrack(rawtoc.Tracks[i].Address, rawtoc.Tracks[i].ControlAndADR.Control, isrc);
           }
           // Next entry should be the leadout (track number 0xAA)
@@ -61,141 +53,126 @@ namespace MetaBrainz.MusicBrainz.DiscId.Platforms {
           tracks[0] = new TableOfContents.RawTrack(rawtoc.Tracks[i].Address, rawtoc.Tracks[i].ControlAndADR.Control, null);
         }
         // TODO: If requested, try getting CD-TEXT data.
-        var mcn = ((features & CdDeviceFeature.ReadMediaCatalogNumber) != 0) ? Windows.GetMediaCatalogNumber(hDevice) : null;
+        var mcn = ((features & CdDeviceFeature.ReadMediaCatalogNumber) != 0) ? NativeApi.GetMediaCatalogNumber(hDevice) : null;
         return new TableOfContents(device, first, last, tracks, mcn);
       }
     }
 
-    private static SafeFileHandle CreateDeviceHandle(string device) {
-      var colon = device.IndexOf(':');
-      if (colon >= 0)
-        device = device.Substring(0, colon + 1);
-      var path = string.Concat("\\\\.\\", device);
-      var handle = Windows.CreateFile(path, FileAccess.Read, FileShare.ReadWrite, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
-      if (handle.IsInvalid) {
-        var error = Marshal.GetLastWin32Error();
-        throw new ArgumentException($"Cannot open the CD audio device '{device}' (error {error:X8}).", new Win32Exception(error));
-      }
-      return handle;
-    }
-
-    private static string GetMediaCatalogNumber(SafeFileHandle hDevice) {
-      var req = new SubChannelRequest { Format = MMC3.SubChannelRequestFormat.MediaCatalogNumber, Track = 0 };
-      var mcn = new MMC3.SubChannelMediaCatalogNumber();
-      var returned = 0;
-      if (!Windows.DeviceIoControl(hDevice, Windows.IOCTL_CDROM_READ_Q_CHANNEL, ref req, Marshal.SizeOf(req), out mcn, Marshal.SizeOf(mcn), out returned, IntPtr.Zero))
-        throw new IOException("Failed to retrieve media catalog number.", new Win32Exception(Marshal.GetLastWin32Error()));
-      mcn.FixUp();
-      return mcn.Status.IsValid ? Encoding.ASCII.GetString(mcn.MCN) : string.Empty;
-    }
-
-    private static string GetTrackIsrc(SafeFileHandle hDevice, byte track) {
-      var req = new SubChannelRequest { Format = MMC3.SubChannelRequestFormat.ISRC, Track = track };
-      var isrc = new MMC3.SubChannelISRC();
-      var returned = 0;
-      if (!Windows.DeviceIoControl(hDevice, Windows.IOCTL_CDROM_READ_Q_CHANNEL, ref req, Marshal.SizeOf(req), out isrc, Marshal.SizeOf(isrc), out returned, IntPtr.Zero))
-        throw new IOException($"Failed to retrieve ISRC for track {track}.", new Win32Exception(Marshal.GetLastWin32Error()));
-      isrc.FixUp();
-      return isrc.Status.IsValid ? Encoding.ASCII.GetString(isrc.ISRC) : string.Empty;
-    }
-
-    #region WinAPI
+    #region Native API
 
     // ReSharper disable InconsistentNaming
-    // ReSharper disable UnusedMember.Local
 
-    #region CreateFile
+    private static class NativeApi {
 
-    [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    private static extern SafeFileHandle CreateFile(string filename, [MarshalAs(UnmanagedType.U4)] FileAccess access, [MarshalAs(UnmanagedType.U4)] FileShare share, IntPtr securityAttributes, [MarshalAs(UnmanagedType.U4)] FileMode mode, uint flags, IntPtr templateFile);
+      #region Constants
 
-    #endregion
-
-    #region DeviceIoControl
-
-    #region Constants
-
-    private const int FILE_DEVICE_CD_ROM = 0x00002;
-
-    private const int FILE_READ_ACCESS = 0x0001;
-
-    private const int METHOD_BUFFERED   = 0;
-    private const int METHOD_IN_DIRECT  = 1;
-    private const int METHOD_OUT_DIRECT = 2;
-    private const int METHOD_NEITHER    = 3;
-
-
-    private const int IOCTL_CDROM_CHECK_VERIFY       = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0200 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_FIND_NEW_DEVICES   = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0206 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_GET_CONTROL        = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x000D << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_GET_DRIVE_GEOMETRY = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0013 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_GET_LAST_SESSION   = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x000E << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_GET_VOLUME         = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0005 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_PAUSE_AUDIO        = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0003 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_PLAY_AUDIO_MSF     = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0006 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_RAW_READ           = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x000F << 2) | Windows.METHOD_OUT_DIRECT);
-    private const int IOCTL_CDROM_READ_Q_CHANNEL     = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x000B << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_READ_TOC           = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0000 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_READ_TOC_EX        = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0015 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_RESUME_AUDIO       = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0004 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_SEEK_AUDIO_MSF     = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0001 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_SET_VOLUME         = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x000A << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_SIMBAD             = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x1003 << 2) | Windows.METHOD_BUFFERED);
-    private const int IOCTL_CDROM_STOP_AUDIO         = ((Windows.FILE_DEVICE_CD_ROM << 16) | (Windows.FILE_READ_ACCESS << 14) | (0x0002 << 2) | Windows.METHOD_BUFFERED);
-
-    #endregion
-
-    #region IOCTL_CDROM_READ_Q_CHANNEL
-
-    [StructLayout(LayoutKind.Sequential)]
-    private struct SubChannelRequest { // aka CDROM_SUB_Q_DATA_FORMAT
-      public MMC3.SubChannelRequestFormat Format;
-      public byte                         Track;
-    }
-
-    [DllImport("Kernel32.dll", SetLastError = true)]
-    private static extern bool DeviceIoControl(SafeFileHandle hDevice, int ioControlCode, ref SubChannelRequest request, int requestSize, out MMC3.SubChannelMediaCatalogNumber data, int dataSize, out int pBytesReturned, IntPtr overlapped);
-
-    [DllImport("Kernel32.dll", SetLastError = true)]
-    private static extern bool DeviceIoControl(SafeFileHandle hDevice, int ioControlCode, ref SubChannelRequest request, int requestSize, out MMC3.SubChannelISRC data, int dataSize, out int pBytesReturned, IntPtr overlapped);
-
-    #endregion
-
-    #region IOCTL_CDROM_READ_TOC_EX
-
-    private struct TOCRequest {
-      public readonly byte FormatInfo;
-      public readonly byte SessionTrack;
-      public readonly byte Reserved1;
-      public readonly byte Reserved2;
-
-      public TOCRequest(MMC3.TOCRequestFormat format = MMC3.TOCRequestFormat.TOC, byte track = 0, bool msf = false) {
-        this.FormatInfo   = (byte) format;
-        this.SessionTrack = track;
-        this.Reserved1    = 0;
-        this.Reserved2    = 0;
-        if (msf)
-          this.FormatInfo |= 0x80;
+      private enum IOCTL : int {
+        CDROM_READ_Q_CHANNEL = 0x2402C,
+        CDROM_READ_TOC_EX    = 0x24054,
       }
 
-      public MMC3.TOCRequestFormat Format => (MMC3.TOCRequestFormat) (this.FormatInfo & 0x0f);
+      #endregion
 
-      public bool AddressAsMSF => (this.FormatInfo & 0x80) == 0x80;
+      #region Structures
+
+      [StructLayout(LayoutKind.Sequential, Pack = 1)]
+      private struct SubChannelRequest { // aka CDROM_SUB_Q_DATA_FORMAT
+        public MMC3.SubChannelRequestFormat Format;
+        public byte                         Track;
+      }
+
+      [StructLayout(LayoutKind.Sequential, Pack = 1)]
+      private struct TOCRequest {
+        public readonly byte FormatInfo;
+        public readonly byte SessionTrack;
+        public readonly byte Reserved1;
+        public readonly byte Reserved2;
+
+        public TOCRequest(MMC3.TOCRequestFormat format = MMC3.TOCRequestFormat.TOC, byte track = 0, bool msf = false) {
+          this.FormatInfo   = (byte) format;
+          this.SessionTrack = track;
+          this.Reserved1    = 0;
+          this.Reserved2    = 0;
+          if (msf)
+            this.FormatInfo |= 0x80;
+        }
+
+        public MMC3.TOCRequestFormat Format => (MMC3.TOCRequestFormat) (this.FormatInfo & 0x0f);
+
+        public bool AddressAsMSF => (this.FormatInfo & 0x80) == 0x80;
+
+      }
+
+      #endregion
+
+      #region Public Methods
+
+      public static string GetMediaCatalogNumber(SafeFileHandle hDevice) {
+        var req = new SubChannelRequest { Format = MMC3.SubChannelRequestFormat.MediaCatalogNumber, Track = 0 };
+        var mcn = new MMC3.SubChannelMediaCatalogNumber();
+        var returned = 0;
+        if (!NativeApi.DeviceIoControl(hDevice, IOCTL.CDROM_READ_Q_CHANNEL, ref req, Marshal.SizeOf(req), out mcn, Marshal.SizeOf(mcn), out returned, IntPtr.Zero))
+          throw new IOException("Failed to retrieve media catalog number.", new Win32Exception(Marshal.GetLastWin32Error()));
+        mcn.FixUp();
+        return mcn.Status.IsValid ? Encoding.ASCII.GetString(mcn.MCN) : string.Empty;
+      }
+
+      public static void GetTableOfContents(SafeFileHandle hDevice, out MMC3.TOCDescriptor rawtoc) {
+        var req = new NativeApi.TOCRequest(MMC3.TOCRequestFormat.TOC);
+        var returned = 0;
+        var rawtoclen = Marshal.SizeOf(typeof(MMC3.TOCDescriptor));
+        // LIB-44: Apparently for some multi-session discs, the first TOC read can be wrong. So issue two reads.
+        var ok = NativeApi.DeviceIoControl(hDevice, IOCTL.CDROM_READ_TOC_EX, ref req, Marshal.SizeOf(req), out rawtoc, rawtoclen, out returned, IntPtr.Zero);
+        if (ok)
+          ok = NativeApi.DeviceIoControl(hDevice, IOCTL.CDROM_READ_TOC_EX, ref req, Marshal.SizeOf(req), out rawtoc, rawtoclen, out returned, IntPtr.Zero);
+        if (!ok)
+          throw new IOException("Failed to retrieve TOC.", new Win32Exception(Marshal.GetLastWin32Error()));
+        rawtoc.FixUp(req.AddressAsMSF);
+      }
+
+      public static string GetTrackIsrc(SafeFileHandle hDevice, byte track) {
+        var req = new SubChannelRequest { Format = MMC3.SubChannelRequestFormat.ISRC, Track = track };
+        var isrc = new MMC3.SubChannelISRC();
+        var returned = 0;
+        if (!NativeApi.DeviceIoControl(hDevice, IOCTL.CDROM_READ_Q_CHANNEL, ref req, Marshal.SizeOf(req), out isrc, Marshal.SizeOf(isrc), out returned, IntPtr.Zero))
+          throw new IOException($"Failed to retrieve ISRC for track {track}.", new Win32Exception(Marshal.GetLastWin32Error()));
+        isrc.FixUp();
+        return isrc.Status.IsValid ? Encoding.ASCII.GetString(isrc.ISRC) : string.Empty;
+      }
+
+      public static SafeFileHandle OpenDevice(string device) {
+        var colon = device.IndexOf(':');
+        if (colon >= 0)
+          device = device.Substring(0, colon + 1);
+        var path = string.Concat("\\\\.\\", device);
+        var handle = NativeApi.CreateFile(path, FileAccess.Read, FileShare.ReadWrite, IntPtr.Zero, FileMode.Open, 0, IntPtr.Zero);
+        if (!handle.IsInvalid)
+          return handle;
+        var error = Marshal.GetLastWin32Error();
+        throw new ArgumentException($"Cannot open device '{device}' (error {error:X8}).", new Win32Exception(error));
+      }
+
+      #endregion
+
+      #region Private Methods
+
+      [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+      private static extern SafeFileHandle CreateFile(string filename, [MarshalAs(UnmanagedType.U4)] FileAccess access, [MarshalAs(UnmanagedType.U4)] FileShare share, IntPtr securityAttributes, [MarshalAs(UnmanagedType.U4)] FileMode mode, uint flags, IntPtr templateFile);
+
+      [DllImport("Kernel32.dll", SetLastError = true)]
+      private static extern bool DeviceIoControl(SafeFileHandle hDevice, IOCTL command, ref SubChannelRequest request, int requestSize, out MMC3.SubChannelMediaCatalogNumber data, int dataSize, out int pBytesReturned, IntPtr overlapped);
+
+      [DllImport("Kernel32.dll", SetLastError = true)]
+      private static extern bool DeviceIoControl(SafeFileHandle hDevice, IOCTL command, ref SubChannelRequest request, int requestSize, out MMC3.SubChannelISRC data, int dataSize, out int pBytesReturned, IntPtr overlapped);
+
+      [DllImport("Kernel32.dll", SetLastError = true)]
+      private static extern bool DeviceIoControl(SafeFileHandle hDevice, IOCTL command, ref TOCRequest request, int nInBufferSize, out MMC3.TOCDescriptor toc, int tocSize, out int pBytesReturned, IntPtr overlapped);
+
+      #endregion
 
     }
 
-    [DllImport("Kernel32.dll", SetLastError = true)]
-    private static extern bool DeviceIoControl(SafeFileHandle hDevice, int ioControlCode, ref TOCRequest request, int nInBufferSize, out MMC3.CDTextDescriptor cdtext, int textSize, out int pBytesReturned, IntPtr overlapped);
-
-    [DllImport("Kernel32.dll", SetLastError = true)]
-    private static extern bool DeviceIoControl(SafeFileHandle hDevice, int ioControlCode, ref TOCRequest request, int nInBufferSize, out MMC3.TOCDescriptor toc, int tocSize, out int pBytesReturned, IntPtr overlapped);
-
     #endregion
-
-    #endregion
-
-    #endregion
-
   }
 
 }
