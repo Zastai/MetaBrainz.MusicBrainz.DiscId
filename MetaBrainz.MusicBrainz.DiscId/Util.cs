14cd60761466adb1aa3bca7f02ce6b930e16a15e
Tim Van Holder <tim.vanholder@gmail.com>
2016-06-19 13:44:42 +0200

    Added utility class. Currently only provides a method to marshal a byte array to a structure.

---
+++ MetaBrainz.MusicBrainz.DiscId/Util.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MetaBrainz.MusicBrainz.DiscId {
+  
+  internal static class Util {
+
+    /// <summary>Marshals a structure from a byte array.</summary>
+    /// <typeparam name="T">The type of structure to marshal.</typeparam>
+    /// <param name="bytes">The raw representation of the structure to marshal.</param>
+    /// <returns>The marshaled structure.</returns>
+    public static T MarshalBytesToStructure<T>(byte[] bytes) where T : struct {
+      var handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+      try {
+        return (T) Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+      }
+      finally {
+        handle.Free();
+      }
+    }
+
+  }
+
+}
